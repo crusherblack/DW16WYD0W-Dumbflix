e3e23aa183ea10828b8f2454826e5a8495581bf6
fadhil darma putera zm <fadhildarma13@gmail.com>
2020-06-04 13:19:43 +0700

    using useHistory for clickable logo dumpflix pada navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,11 +6,19 @@ import logo from "../../img/logo.png";
 import person from "../../img/person.jpg";
 import ProfileDropdown from "../Profile/ProfileDropdown";
 
+import { useHistory } from "react-router-dom";
+
 const Navbar = ({ isLogin, showModalLogin, showModalRegister }) => {
+  let history = useHistory();
+
   const [isProfileDropdown, setProfileDropdown] = useState(false);
 
   const showProfileDropdown = () => {
     setProfileDropdown(!isProfileDropdown);
+  };
+
+  const openHome = () => {
+    history.push(`/`);
   };
 
   return (
@@ -19,7 +27,12 @@ const Navbar = ({ isLogin, showModalLogin, showModalRegister }) => {
       <Link to="/tv-series">TV Shows</Link>
       <Link to="/movies">Movies</Link>
       <div className="logo">
-        <img src={logo} alt="" />
+        <img
+          src={logo}
+          alt="dumbflix"
+          style={{ cursor: "pointer" }}
+          onClick={() => openHome()}
+        />
       </div>
       {!isLogin && (
         <div className="button-login-register">
