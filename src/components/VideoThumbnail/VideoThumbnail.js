4c02e5dacee746b2331eda46f6decde6ea48943d
crusherblack <fadhildarma13@gmail.com>
2020-06-05 11:28:13 +0700

    add react youtube player along with react video player

--- src/components/VideoThumbnail/VideoThumbnail.js
+++ src/components/VideoThumbnail/VideoThumbnail.js
@@ -2,21 +2,34 @@ import React from 'react';
 import 'video-react/dist/video-react.css';
 import { Player, BigPlayButton, ControlBar } from 'video-react';
 import './VideoThumbnail.css';
+import YouTube from '@u-wave/react-youtube';
 
 const VideoThumbnail = ({ thumbnail }) => {
+	let type = 'youtube';
+
 	return (
 		<div className="video">
-			<Player
-				poster={thumbnail}
-				height={536}
-				width="100%"
-				fluid={false}
-				autoPlay={false}
-			>
-				<source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-				<BigPlayButton position="center" />
-				<ControlBar autoHide={true} />
-			</Player>
+			{type == 'youtube' ? (
+				<YouTube
+					video="ndl1W4ltcmg"
+					width="100%"
+					height={536}
+					controls={false}
+					autoplay
+				/>
+			) : (
+				<Player
+					poster={thumbnail}
+					height={536}
+					width="100%"
+					fluid={false}
+					autoPlay={false}
+				>
+					<source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+					<BigPlayButton position="center" />
+					<ControlBar autoHide={true} />
+				</Player>
+			)}
 		</div>
 	);
 };
