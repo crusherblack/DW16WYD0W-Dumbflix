eba69dc2e7a3be006f3de5465fe30d267c9321db
crusherblack <fadhildarma13@gmail.com>
2020-06-05 09:42:12 +0700

    fix routing protection

--- src/pages/IncomingTransaction.js
+++ src/pages/IncomingTransaction.js
@@ -1,62 +1,65 @@
-import React from "react";
-import "./css/Transcation.css";
-import { transaction } from "../fakedata/transcation";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import './css/Transcation.css';
+import { transaction } from '../fakedata/transcation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 const IncomingTransaction = () => {
-  const data = transaction.map((data, index) => (
-    <tr key={data.id}>
-      <td>{index + 1}</td>
-      <td>{data.user}</td>
-      <td>{data.bukti}</td>
-      <td>{data.remaining} Hari</td>
-      <td
-        style={{
-          color: data.status == "active" ? "#0ACF83" : "#FF0742",
-        }}
-      >
-        {data.status}
-      </td>
-      <td
-        style={{
-          color:
-            data.status_payment == "approve"
-              ? "#0ACF83"
-              : data.status_payment == "pending"
-              ? "#F7941E"
-              : "#FF0742",
-        }}
-      >
-        {data.status_payment}
-      </td>
-      <td>
-        <span style={{ fontSize: "20px", cursor: "pointer", color: "#1C9CD2" }}>
-          <FontAwesomeIcon icon={faCaretDown} />
-        </span>
-      </td>
-    </tr>
-  ));
+	const data = transaction.map((data, index) => (
+		<tr key={data.id}>
+			<td>{index + 1}</td>
+			<td>{data.user}</td>
+			<td>{data.bukti}</td>
+			<td>{data.remaining} Hari</td>
+			<td
+				style={{
+					color: data.status == 'active' ? '#0ACF83' : '#FF0742'
+				}}
+			>
+				{data.status}
+			</td>
+			<td
+				style={{
+					color:
+						data.status_payment == 'approve'
+							? '#0ACF83'
+							: data.status_payment == 'pending' ? '#F7941E' : '#FF0742'
+				}}
+			>
+				{data.status_payment}
+			</td>
+			<td>
+				<span style={{ fontSize: '20px', cursor: 'pointer', color: '#1C9CD2' }}>
+					<div className="dropdown">
+						<div className="dropdown-content">
+							<label htmlFor="">Active</label>
+						</div>
+						<FontAwesomeIcon icon={faCaretDown} />
+					</div>
+				</span>
+			</td>
+		</tr>
+	));
 
-  return (
-    <div className="transcation-container">
-      <h1>Incoming Transcation</h1>
-      <table class="transcation-table">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Users</th>
-            <th>Bukti Transfer</th>
-            <th>Remaining Aktif</th>
-            <th>Status User</th>
-            <th>Status Payment</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>{data}</tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div className="transcation-container">
+			<h1>Incoming Transcation</h1>
+			<table className="transcation-table">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Users</th>
+						<th>Bukti Transfer</th>
+						<th>Remaining Aktif</th>
+						<th>Status User</th>
+						<th>Status Payment</th>
+						<th>Aksi</th>
+					</tr>
+				</thead>
+				<tbody>{data}</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default IncomingTransaction;
