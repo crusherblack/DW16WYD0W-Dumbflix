534611a55a71a5f659f1f0811ecefa91fc1eb675
fadhil darma putera zm <fadhildarma13@gmail.com>
2020-06-08 10:16:02 +0700

    fix card

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import './MovieCard.css';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./MovieCard.css";
 
 const MovieCard = ({ movie: { id, name, year, img } }) => {
-	let history = useHistory();
+  let history = useHistory();
 
-	return (
-		<div>
-			<img
-				className="card"
-				src={img}
-				onClick={() => history.push(`/detail/${id}`)}
-				style={{ height: '22em' }}
-			/>
-			<span className="movie-title">{name}</span>
-			<span className="movie-year">{year}</span>
-		</div>
-	);
+  return (
+    <div>
+      <img
+        className="card"
+        src={img}
+        onClick={() => history.push(`/detail/${id}`)}
+        style={{ height: "auto" }}
+      />
+      <span className="movie-title">{name}</span>
+      <span className="movie-year">{year}</span>
+    </div>
+  );
 };
 
 export default MovieCard;
--- src/pages/TVSection.js
+++ src/pages/TVSection.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import HeroImage from '../components/HeroImage/HeroImage';
-import MovieGrid from '../components/MovieGrid/MovieGrid';
-import { heroImage2 } from '../fakedata/heroimage';
-import { movieList } from '../fakedata/movie';
+import React from "react";
+import HeroImage from "../components/HeroImage/HeroImage";
+import MovieGrid from "../components/MovieGrid/MovieGrid";
+import { heroImage2 } from "../fakedata/heroimage";
+import { movieList } from "../fakedata/movie";
 
 const TVSection = () => {
-	return (
-		<div>
-			<HeroImage heroImage={heroImage2} />
-			<MovieGrid movieList={movieList} title="TV Series" filter={12} type="tv" />
-		</div>
-	);
+  return (
+    <div>
+      <HeroImage heroImage={heroImage2} />
+      <MovieGrid
+        movieList={movieList}
+        title="TV Series"
+        filter={12}
+        type="tv"
+      />
+    </div>
+  );
 };
 
 export default TVSection;
