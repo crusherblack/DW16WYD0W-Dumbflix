f03068ba1ce1ed4f4ae86bca5467f09b2925a466
fadhil darma putera zm <fadhildarma13@gmail.com>
2020-06-08 11:22:00 +0700

    fix detail css

--- src/pages/DetailMovie.js
+++ src/pages/DetailMovie.js
@@ -1,65 +1,65 @@
-import React from 'react';
-import { movieList } from '../fakedata/movie';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { movieList } from "../fakedata/movie";
+import { useHistory } from "react-router-dom";
 
-import VideoThumbnail from '../components/VideoThumbnail/VideoThumbnail';
-import Description from '../components/MovieDetail/Description';
-import Episode from '../components/MovieDetail/Episode';
-import Movie from '../components/MovieDetail/Movie';
-import './css/DetailMovie.css';
+import VideoThumbnail from "../components/VideoThumbnail/VideoThumbnail";
+import Description from "../components/MovieDetail/Description";
+import Episode from "../components/MovieDetail/Episode";
+import Movie from "../components/MovieDetail/Movie";
+import "./css/DetailMovie.css";
 
-import moviesThumbnail from '../img/videothumbnail/video2.png';
-import tvThumbnail from '../img/videothumbnail/video1.png';
+import moviesThumbnail from "../img/videothumbnail/video2.png";
+import tvThumbnail from "../img/videothumbnail/video1.png";
 
 const DetailMovie = ({ match }) => {
-	let history = useHistory();
-	let role = 'admin';
-	const movie = movieList.filter((detail) => {
-		return detail.id == match.params.id;
-	});
+  let history = useHistory();
+  let role = "admin";
+  const movie = movieList.filter((detail) => {
+    return detail.id == match.params.id;
+  });
 
-	const openAddEpisode = () => {
-		history.push('/add-episode');
-	};
+  const openAddEpisode = () => {
+    history.push("/add-episode");
+  };
 
-	return (
-		<div style={{ marginTop: '70px' }}>
-			<VideoThumbnail
-				thumbnail={movie[0].type == 'tv' ? tvThumbnail : moviesThumbnail}
-			/>
-			{role == 'admin' && movie[0].type == 'tv' ? (
-				<div
-					style={{
-						height: '20px',
-						position: 'relative'
-					}}
-				>
-					<button
-						className="btn-merah"
-						style={{
-							float: 'right',
-							width: '12em',
-							top: '1em',
-							right: '2em',
-							marginBottom: '3em'
-						}}
-						onClick={() => openAddEpisode()}
-					>
-						Add Episode
-					</button>
-				</div>
-			) : null}
+  return (
+    <div style={{ marginTop: "70px" }}>
+      <VideoThumbnail
+        thumbnail={movie[0].type == "tv" ? tvThumbnail : moviesThumbnail}
+      />
+      {role == "admin" && movie[0].type == "tv" ? (
+        <div
+          style={{
+            height: "50px",
+            position: "relative",
+          }}
+        >
+          <button
+            className="btn-merah"
+            style={{
+              float: "right",
+              width: "12em",
+              top: "1em",
+              right: "2em",
+              marginBottom: "3em",
+            }}
+            onClick={() => openAddEpisode()}
+          >
+            Add Episode
+          </button>
+        </div>
+      ) : null}
 
-			<div className="details">
-				<Description movie={movie[0]} />
-				{movie[0].type == 'tv' ? (
-					<Episode name={movie[0].name} />
-				) : (
-					<Movie name={movie[0].name} />
-				)}
-			</div>
-		</div>
-	);
+      <div className="details">
+        <Description movie={movie[0]} />
+        {movie[0].type == "tv" ? (
+          <Episode name={movie[0].name} />
+        ) : (
+          <Movie name={movie[0].name} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default DetailMovie;
